9b1b1dd2016728186cacafe92d114eca06ea03ff
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-09 14:18:27 +0530

    added login serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,1 +1,40 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs['email']
+        password = attrs['password']
+
+        user = authenticate(
+            username=email,
+            password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid Username or Password"
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                "This account is inactive"
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return{
+            'access':str(refresh.access_token),
+            'refresh':str(refresh),
+            'user':{
+                'id':user.id,
+                'username':user.username,
+                'email':user.email,
+                'role':user.role
+            },
+        }
