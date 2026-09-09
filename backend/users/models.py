f3c7624e8e437c21af28da0a916f3ee585fa9df4
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-09 14:22:40 +0530

    changed  USERNAME_FIELD = 'email'     REQUIRED_FIELDS = ['username'] in user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -10,6 +10,9 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
@@ -34,4 +37,6 @@ class User(AbstractUser):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    
 
