1993d48fae66f466ead5b8fcb4ca2fe9a9c1aa90
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-09 12:15:46 +0530

    added department model

--- backend/department/models.py
+++ backend/department/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    upgraded_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
