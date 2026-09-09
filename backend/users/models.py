0013eedec1d5d3b154c7a4427355a1d9099e93dc
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-09 12:30:06 +0530

    added user table

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN","Admin"
+        MANAGER = "MANAGER","Manager"
+        EMPLOYEE = "EMPLOYEE","Employee"
+
+    email = models.EmailField(unique=True)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.EMPLOYEE,
+    )
+
+    department = models.ForeignKey(
+        "department.Department",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='employees'
+    )
+
+    manager = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="team_members"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
