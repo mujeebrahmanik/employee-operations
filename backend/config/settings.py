454a8fe9b199dee710eddd3a080d56322399baec
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-09 14:57:21 +0530

    updated auth backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,7 +139,7 @@ AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "users.authentication.CookieJWTAuthentication",
     ),
 }
 
