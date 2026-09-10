77e69c4346528a7c1535f425606ef4049065fd51
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-10 11:46:27 +0530

    added success login test case

---
+++ backend/users/tests/test_auth.py
@@ -0,0 +1,28 @@
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+@pytest.mark.django_db
+def test_login_success():
+    user = User.objects.create_user(
+        username = 'testuser',
+        email = 'test@example.com',
+        password = 'testpassword123'
+    )
+
+    client = APIClient()
+
+    response = client.post(
+        '/api/auth/login/',{
+            'email':'test@example.com',
+            'password':'testpassword123'
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.data['user']['email'] == user.email
+
+    assert 'access_token' in response.cookies
+    assert 'refresh_token' in response.cookies 
