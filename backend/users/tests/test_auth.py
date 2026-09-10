ec6e266e28c3c05d49f9a562761c7e662a7ea6da
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-09-10 12:03:39 +0530

    added login non existing email test case

--- backend/users/tests/test_auth.py
+++ backend/users/tests/test_auth.py
@@ -47,3 +47,18 @@ def test_with_wrong_password():
     )
 
     assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_login_nonesistent_email():
+    client = APIClient()
+
+    response = client.post(
+        '/api/auth/login/',
+        {
+            'email':'nonexistant@example.com',
+            'password':'testpassword123'
+        }
+    )
+
+    assert response.status_code == 400
